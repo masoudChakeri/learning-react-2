4958d63d41f170fa1da3bb42b120adfad93b512f
masoudChakeri <masoud.chakeri@gmail.com>
2023-03-13 13:20:52 +0330

    add MealItem

--- src/Meals/AvailableMeals.js
+++ src/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
 import classes from "./AvailableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
+import Card from "../UI/Card";
 
 const DUMMY_MEALS = [
     {
@@ -29,14 +31,16 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
     const mealsList = DUMMY_MEALS.map(meal =>
-        <li>{meal.name}</li>
+        <MealItem key={meal.id} meal={meal}/>
     )
 
     return (
         <section className={classes.meals}>
-            <ul>
-                {mealsList}
-            </ul>
+            <Card>
+                <ul>
+                    {mealsList}
+                </ul>
+            </Card>
         </section>
     )
 }
