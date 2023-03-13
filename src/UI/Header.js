dde9556c0155c5dabc879f7bb3cde595dd9b3f19
masoudChakeri <masoud.chakeri@gmail.com>
2023-03-13 12:17:07 +0330

    add HeaderCartButton

--- src/UI/Header.js
+++ src/UI/Header.js
@@ -2,14 +2,14 @@ import classes from "./Header.module.css";
 import {Fragment} from "react";
 
 import meals from "../Assets/meals.jpg"
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = () => {
     return (
         <Fragment>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <button type="submit">Cart</button>
-                {/*<button type="submit">asdasd</button>*/}
+                <HeaderCartButton/>
             </header>
 
             <div className={classes['main-image']}>
