bfaff7ffc74a81e8dff70ad33e5d4feae3f14da1
masoudChakeri <masoud.chakeri@gmail.com>
2023-03-13 12:59:53 +0330

    add meals components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import {Fragment} from "react";
 import Header from "./UI/Header";
+import Meals from "./Meals/Meals";
 
 function App() {
     return (
         <Fragment>
             <Header/>
+            <main>
+                <Meals/>
+            </main>
         </Fragment>
     );
 }
---
+++ src/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+    return (
+        <section>
+            <MealsSummary/>
+            <AvailableMeals/>
+        </section>
+    )
+}
+
+export default Meals
